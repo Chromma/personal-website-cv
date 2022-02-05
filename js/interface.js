3e98b28d41a14c70952b59d6208925653b183a17
Kevin Serrano <serranokevin22@gmail.com>
2022-02-04 23:50:45 -0500

    Base Website
    Base website format #1

---
+++ js/interface.js
@@ -0,0 +1,15 @@
+﻿( function($) {
+  'use strict';
+  	
+
+/* Carousel experience*/
+    $('.carousel-experience').owlCarousel({
+        loop:true,
+        margin:45,
+        dots:true,
+        nav:true,
+        smartSpeed:1000,
+        items:1
+    });
+
+})(jQuery);
